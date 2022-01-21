59cf393a54e714cb43e01a44738f7168922b1218
MindFaktur <69393019+MindFaktur@users.noreply.github.com>
2022-01-21 18:31:32 +0530

    Update user_inputs.py

--- employee_wage/emp_user_inputs/user_inputs.py
+++ employee_wage/emp_user_inputs/user_inputs.py
@@ -6,19 +6,19 @@ class Employee:
     Creates a new employee object with all the given parameters
     """
 
-    def __init__(self, part_time_hr, full_time_hr, max_work_days,
-                 max_work_hrs, wage_per_hr, company_name):
-        self.part_time_hr = part_time_hr
-        self.full_time_hr = full_time_hr
-        self.max_work_days = max_work_days
-        self.max_work_hrs = max_work_hrs
+    def __init__(self, company_dict):
+        self.part_time_hr = company_dict["part_time_hr"]
+        self.full_time_hr = company_dict["full_time_hr"]
+        self.max_work_days = company_dict["max_work_days"]
+        self.max_work_hrs = company_dict["max_work_hrs"]
         self.total_part_time_days = 0
         self.total_full_time_days = 0
         self.total_absent_days = 0
-        self.wage_per_hr = wage_per_hr
-        self.company_name = company_name
+        self.wage_per_hr = company_dict["wage_per_hr"]
+        self.company_name = company_dict["company_name"]
         self.total_wage = 0
         self.max_hrs_worked = 0
+        self.wage_per_day = {}
 
     def __str__(self):
         """
@@ -36,6 +36,7 @@ class Employee:
                ", total_absent_days: " + str(self.total_absent_days) + \
                ", total_wage: " + str(self.total_wage) + \
                ", max_hrs_worked: " + str(self.max_hrs_worked) + \
+               ", daily_wage: " + str(self.wage_per_day) + \
                " }"
 
     @staticmethod
@@ -66,26 +67,30 @@ class Employee:
         total_part_time_days = 0
         total_absent_days = 0
         max_days_worked = total_full_time_days + total_part_time_days + total_absent_days
-
+        i = 0
         while (total_part_time_days + total_full_time_days) <= self.max_work_days \
                 and max_hrs_worked <= self.max_work_hrs \
                 and max_days_worked <= 31:
             type_of_day = self.returns_random_number()
             if type_of_day == 0:
+                self.wage_per_day["day " + str(i)] = 0
                 total_absent_days = total_absent_days + 1
+                i = i + 1
             elif type_of_day == 1:
-                total_wage = total_wage + self.emp_wage_for_the_day(self.part_time_hr)
+                self.wage_per_day["day " + str(i)] = self.emp_wage_for_the_day(self.part_time_hr)
+                total_wage = total_wage + self.wage_per_day["day " + str(i)]
                 total_part_time_days = total_part_time_days + 1
                 max_hrs_worked = max_hrs_worked + self.part_time_hr
+                i = i + 1
             else:
-                total_wage = total_wage + self.emp_wage_for_the_day(self.full_time_hr)
+                self.wage_per_day["day " + str(i)] = self.emp_wage_for_the_day(self.full_time_hr)
+                total_wage = total_wage + self.wage_per_day["day " + str(i)]
                 total_full_time_days = total_full_time_days + 1
                 max_hrs_worked = max_hrs_worked + self.full_time_hr
-
+                i = i + 1
+        self.wage_per_day["Total Wage"] = total_wage
         self.total_wage = total_wage
         self.max_hrs_worked = max_hrs_worked
         self.total_full_time_days = total_full_time_days
         self.total_part_time_days = total_part_time_days
         self.total_absent_days = total_absent_days
-
-
